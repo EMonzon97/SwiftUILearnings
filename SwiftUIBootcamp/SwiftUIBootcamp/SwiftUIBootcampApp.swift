a891a6cf35c0ddfb733347bffeae4d7beb38b68a
Eliseo Monzon <elimonzon97@gmail.com>
2024-02-24 16:42:51 -0500

    Swift UI First Commit - Create Folder for SwiftUI Bootcamp

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Eliseo Monzon on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
